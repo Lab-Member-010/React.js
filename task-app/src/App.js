aaa3cff45fa21b8f8076c3bc2f806d04acc6ac62
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-01-04 15:06:38 +0530

    task input update

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -12,6 +12,19 @@ class App extends Component {
   render() {
     return <>
       <Header />
+      {/* <div className="container"> */}
+        <div className="col-md-6">
+          <input type="text" id="task" placeholder="Enter Task Title"/>
+        </div>
+        <div className="col-md-6">
+          <select id="priority">
+            <option>Low</option>
+            <option>Medium</option>
+            <option>High</option>
+          </select>
+        </div>
+        <div className="col-md-6"></div>
+      {/* </div> */}
     </>
   }
 }
