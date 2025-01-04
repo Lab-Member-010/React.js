52364e054c6e561ee038de4982e2357c9cab0dc9
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-01-04 21:16:18 +0530

    new change

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,29 +1,46 @@
 import { Component } from "react";
 import Header from "./components/Header";
-import {data} from "./data";
+import { data } from "./data";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      taskList: data
+      taskList: data,
+      priorityList:["high","medium","low"]
     };
   }
   render() {
     return <>
       <Header />
-      <div className="container row">
-        <div className="col-md-6">
-          <input type="text" id="task" placeholder="Enter Task Title"/>
+      <div className='container'>
+        <div className="row mt-3">
+          <div className='col-md-6'>
+            <input ref={(taskInput) => (this.tittle = taskInput)} className='form-control' type='text' placeholder='Enter tast tittle'></input>
+          </div>
+          <div className='col-md-6'>
+            <select ref={(priorityInput) => (this.priority = priorityInput)} className="form-control">
+              {this.state.priorityList.map((priority, index) => (
+                <option key={index}>{priority}</option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div className="col-md-6">
-          <select id="priority">
-            <option>Low</option>
-            <option>Medium</option>
-            <option>High</option>
-          </select>
+        <div className="row mt-3">
+          <div className="col-md-6">
+            <button className="btn btn-success" onClick={this.addRecord}>
+              ADD
+            </button>
+          </div>
         </div>
-        <div className="col-md-6"></div>
+        <div className="row mt-3">
+          <div className='col-md-3'>
+          <button className="btn btn-info">Active</button>
+          </div>
+          <div className='col-md-3'>
+            <button className="btn btn-danger">Deactive</button>
+          </div>
+        </div>
       </div>
     </>
   }
